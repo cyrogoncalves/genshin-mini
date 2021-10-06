e7cecaaa53eee50497d8f3aa232730a017204e49
cyro <cyro.goncalves@globalhitss.com.br>
2021-10-06 17:50:00 -0300

    gacha tests

--- src/gacha.test.ts
+++ src/gacha.test.ts
@@ -1,13 +1,56 @@
 import { assertEquals } from "https://deno.land/std@0.105.0/testing/asserts.ts";
 import { kokomiBanner, gachaPull, UserHistory } from "./gacha.ts"
 
+const createHistory = (): UserHistory => ({
+  5: { counter: 0, poolCounter: 0 },
+  4: { counter: 0, poolCounter: 0 },
+  3: { counter: 0, poolCounter: 0 }
+});
+
 Deno.test("Pull 3 star", () => {
-  const hist : UserHistory = {
-    5: { counter: 0, poolCounter: 0 },
-    4: { counter: 0, poolCounter: 0 },
-    3: { counter: 0, poolCounter: 0 }
-  };
-  const name = gachaPull(hist, kokomiBanner, 0.1);
+  const hist = createHistory();
+  const name = gachaPull(hist, kokomiBanner, .1);
   assertEquals(name, "Recurve Bow");
 });
+Deno.test("Pull feature 4 star without pity", () => {
+  const hist = createHistory();
+  const name = gachaPull(hist, kokomiBanner, .04, .1, .1);
+  assertEquals(name, "Xingqiu");
+});
+Deno.test("Pull 3 star on 8th pull", () => {
+  const hist = createHistory();
+  hist[4].counter = 7;
+  const name = gachaPull(hist, kokomiBanner, .1);
+  assertEquals(name, "Recurve Bow");
+});
+Deno.test("Pull feature 4 star on 9th pull", () => {
+  const hist = createHistory();
+  hist[4].counter = 8;
+  const name = gachaPull(hist, kokomiBanner, .1, .1, .1);
+  assertEquals(name, "Xingqiu");
+});
+Deno.test("Pull 3 star on 9th pull", () => {
+  const hist = createHistory();
+  hist[4].counter = 8;
+  const name = gachaPull(hist, kokomiBanner, .7);
+  assertEquals(name, "Recurve Bow");
+});
+Deno.test("Pull guaranteed 4 star on 10th pull", () => {
+  const hist = createHistory();
+  hist[4].counter = 9;
+  const name = gachaPull(hist, kokomiBanner, 1, .1, .1);
+  assertEquals(name, "Xingqiu");
+});
+Deno.test("Pull basic 4 star without pool pity", () => {
+  const hist = createHistory();
+  const name = gachaPull(hist, kokomiBanner, .04, .6, .01);
+  assertEquals(name, "Amber");
+});
+Deno.test("Pull guaranteed feature 4 star on pool pity", () => {
+  const hist = createHistory();
+  hist[4].poolCounter = 1;
+  const name = gachaPull(hist, kokomiBanner, .04, .6, .01);
+  assertEquals(name, "Xingqiu");
+});
+
 // deno test --no-check src/gacha.test.ts
--- src/gacha.ts
+++ src/gacha.ts
@@ -8,30 +8,15 @@ const threeStarWeapons = ["Recurve Bow"]; // TODO
 
 const fourStarPool = [...fourStarChars, ...fourStarWeapons];
 
-const rates = [
-  { rank: 5, rate: .006, pity: 73, pool: [...fiveStarChars, ...fiveStarWeapons] },
-  { rank: 4, rate: .051, pity: 8, pool: [...fourStarChars, ...fourStarWeapons] },
-  { rank: 3, rate: 1, pity: 1, pool: threeStarWeapons }
-];
-
 const standardBanner = [
   { rank: 5, rate: .006, pity: 73, pools: [{ pool: [...fiveStarChars, ...fiveStarWeapons] }] },
   { rank: 4, rate: .051, pity: 8, pools: [{ pool: fourStarPool }] },
   { rank: 3, pools: [{ pool: threeStarWeapons }] }
 ];
 
-const kkomiBanner = [
-  { rank: 5, rate: .006, pity: 73, pools: [
-      { pool: ["Kokomi"], rate: .5 }, { pool: fiveStarChars }]},
-  { rank: 4, rate: .051, pity: 8, pools: [
-      { pool: ["Xingqiu", "Beidou", "Rosaria"], rate: .5 },
-      { pool: [...fourStarChars, ...fourStarWeapons] }] },
-  { rank: 3, pools: [{ pool: threeStarWeapons }] }
-];
-
 const createCharacterBanner = (feature5Stars: string[], feature4Stars: string[]) => [
   { rank: 5, rate: .006, pity: 73, pools: [{ pool: feature5Stars, rate: .5 }, { pool: fiveStarChars }]},
-  { rank: 4, rate: .051, pity: 8, pools: [{ pool: feature4Stars, rate: .5 }, { pool: fourStarPool }] },
+  { rank: 4, rate: .051, pity: 7, pools: [{ pool: feature4Stars, rate: .5 }, { pool: fourStarPool }] },
   { rank: 3, pools: [{ pool: threeStarWeapons }] }
 ];
 
@@ -44,6 +29,8 @@ const createWeaponBanner = (feature5Stars: string[], feature4Stars: string[]) =>
 ];
 
 export const kokomiBanner = createCharacterBanner(["Kokomi"], ["Xingqiu", "Beidou", "Rosaria"]);
+export const donutBanner = createWeaponBanner(["Everlasting Moonglow", "Primordial Jade Cutter"],
+    ["Stringless", "The Flute", "Favonius Codex", "Dragon's Bane", "Favonius Greatsword"]);
 
 export type UserHistory = {
   [rate: number]: {
@@ -54,15 +41,6 @@ export type UserHistory = {
   }
 }
 
-const getDrop = (banner, counters: UserHistory) => {
-  const x = Math.random();
-  let sumRate = 0;
-  return banner.find(({ rank, rate = 1, pity = 0 }) => {
-    sumRate += rate + Math.max(0, (counters[rank].counter - pity) * 10 * rate);
-    return x < sumRate;
-  });
-}
-
 export const gachaPull = (
     counters: UserHistory,
     banner = standardBanner,
@@ -71,28 +49,15 @@ export const gachaPull = (
     pickRng = Math.random()
 ) => {
   let sumRate = 0;
-  const drop = banner.find(({ rank, rate = 1, pity = 0 }) => {
-    sumRate += rate + Math.max(0, (counters[rank]?.counter - pity) * 10 * rate);
-    return rankRng < sumRate;
-  });
+  const drop = banner.find(({ rank, rate = 1, pity = 0 }) =>
+      rankRng < (sumRate += rate + Math.max(0, (counters[rank]?.counter - pity) * 10 * rate)));
   const counter = counters[drop.rank];
 
-  Object.keys(counters).forEach(k => counters[k].counter++);
-  counter.counter = 0;
-
   let poolSumRate = 0;
-  const pool = drop.pools.find(({ rate = 1, pity = 0 }) => {
-    poolSumRate += rate + Math.max(0, (counter.poolCounter - pity) * 10 * rate);
-    return poolRng < poolSumRate;
-  });
-  // console.log( { drop, counter, pool });
-  // counter.poolCounter = (counter.poolCounter + 1) % pool.length;
-  if (pool.choicePity && counter.featureChoiceCounter++ > pool.choicePity) {
-    counter.featureChoiceCounter = 0;
-    counter.poolCounter = 0;
-    return pool.pool[counter.featureChoice];
-  }
-  return pool.pool[Math.floor(pickRng * pool.pool.length)];
-}
+  const pool = drop.pools.find(({ rate = 1, pity = 0 }) =>
+      poolRng < (poolSumRate += rate + Math.max(0, (counter.poolCounter - pity) * 10 * rate)));
 
-// featureChoice && poolCounter > choicePity ? featureChoice : Math.floor(Math.random() * (poolCounter > poolPity ? poolPityLength : pool.length))
+  const pick = pool.choicePity && counter.featureChoiceCounter++ > pool.choicePity
+      ? counter.featureChoice : Math.floor(pickRng * pool.pool.length)
+  return pool.pool[pick];
+}
